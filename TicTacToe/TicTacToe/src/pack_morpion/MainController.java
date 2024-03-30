6e2d4935d591bd3c46910150d7e90543d4ea5de6
Romain.Allegre <101862702+RomainAllegre@users.noreply.github.com>
2024-03-30 17:12:38 +0100

    ajout d'un style Model

---
+++ TicTacToe/TicTacToe/src/pack_morpion/MainController.java
@@ -0,0 +1,29 @@
+package pack_morpion;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+
+
+public class MainController {
+
+    @FXML
+    private BorderPane borderPane;
+    
+    
+
+    @FXML
+    private void handlePlayButtonClicked() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("VersusLayout.fxml"));
+            Parent root = loader.load();
+            borderPane.setCenter(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
